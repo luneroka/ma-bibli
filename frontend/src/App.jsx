0e5d037c3b4186ea5e6564a38aa8b8e3933468cb
Yoann Robert <185387760+luneroka@users.noreply.github.com>
2025-02-04 14:43:38 +0100

    Render Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from 'react-router';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      <nav>Navbar</nav>
+      <Navbar />
       <main className='min-h-screen max-w-screen-2x1 mx-auto px-4 py-4 font-lato'>
         <Outlet />
       </main>
