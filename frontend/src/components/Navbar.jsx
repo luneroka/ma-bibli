62e8f4528be313531f8672a2747867f0405dd125
Yoann Robert <185387760+luneroka@users.noreply.github.com>
2025-02-04 15:43:37 +0100

    Import text variables and finish styling navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,28 +7,32 @@ import { IoSearchOutline } from 'react-icons/io5';
 
 const Navbar = () => {
   return (
-    <header className='max-w-screen-2x1 mx-[128px] px-4 py-6'>
-      <nav className='flex justify-between items-center'>
+    <header className='max-w-screen-2x1'>
+      <nav className='flex justify-between items-center bg-main-blue  px-[128px] py-[32px]'>
         {/* left side */}
-        <div>
+        <div className='flex items-center md:gap-4'>
           {/* Search Input */}
-          <div>
-            <IoSearchOutline />
+          <div className='relative w-full min-w-[480px] max-w-[480px]'>
+            <IoSearchOutline className='absolute inline-block left-3 inset-y-0 my-auto' />
             <input
               type='text'
               placeholder='Rechercher un livre ou un auteur...'
-              className='bg-white-bg w-full py-1 md:px-8 px-6 round-md focus:outline-none'
+              className='bg-white-bg w-full h-10 pl-10 pr-4 rounded-lg focus:outline-none'
             />
           </div>
+          {/* Search Button */}
+          <button className='font-merriweather text-white-bg bg-primary-btn px-6 h-10 rounded-lg text-h6'>
+            Search
+          </button>
         </div>
 
         {/* right side */}
-        <div>
+        <div className='flex justify-between items-center gap-[24px]'>
           <Link to='/'>
-            <FaListAlt className='w-[32px] h-[32px] text-primary-btn' />
+            <FaListAlt className='w-[24px] h-[24px] text-white-bg' />
           </Link>
           <Link to='/'>
-            <FaBookOpen className='w-[32px] h-[32px] text-primary-btn' />
+            <FaBookOpen className='w-[24px] h-[24px] text-white-bg' />
           </Link>
           <Link to='/'>
             <FaUser className='w-[32px] h-[32px] text-primary-btn' />
