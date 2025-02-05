afdb259a313f15a004f0ad417d9a6bdad3ae3542
Yoann Robert <185387760+luneroka@users.noreply.github.com>
2025-02-05 16:18:33 +0100

    Style and center login form

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 function Login() {
   return (
-    <div id='login-form' className='flex items-center justify-center h-screen'>
-      <div>
-        <h2 className='text-h4 font-merriweather'>Mon espace lecture</h2>
+    <div className='flex flex-col flex-1 min-h-0 max-w-full mx-auto font-lato'>
+      <div className='flex-grow flex items-center justify-center mt-[96px]'>
+        <form className='bg-white p-8 rounded-lg shadow-md w-full max-w-md'>
+          <h2 className='text-h5 text-black'>Mon espace lecture</h2>
+        </form>
       </div>
     </div>
   );
