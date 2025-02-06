6c003929f6bc4ceccad0da1fa87f111b9029c0b7
Yoann Robert <185387760+luneroka@users.noreply.github.com>
2025-02-06 20:34:19 +0100

    Rectify schema model and start creating add-book route

--- backend/src/books/book.model.js
+++ backend/src/books/book.model.js
@@ -1,44 +1,48 @@
 const mongoose = require('mongoose');
 
-const bookSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    authors: {
+      type: [String], // Better to be explicit with the type
+      required: true,
+    },
+    publisher: {
+      type: String,
+      required: true,
+    },
+    publishedDate: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    pageCount: {
+      type: Number, // Using Number is better for numeric values
+      required: true,
+    },
+    categories: {
+      type: [String], // Explicit array type
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  authors: {
-    type: Array,
-    required: true,
-  },
-  publisher: {
-    type: String,
-    required: true,
-  },
-  publishedDate: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  pageCount: {
-    type: String,
-    required: true,
-  },
-  categories: {
-    type: Array,
-    required: true,
-  },
-  thumbnail: {
-    type: String,
-    required: true,
-  },
-  dateCreated: {
-    type: Date,
-    default: Date.now,
-  },
-  timestamps: true,
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Book = mongoose.model('Book', bookSchema);
 
--- backend/src/books/book.route.js
+++ backend/src/books/book.route.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const Book = require('./book.model');
 const router = express.Router();
 
 router.post('/add-book', async (req, res) => {
-  
-})
+  try {
+    const newBook = await Book({ ...req.body });
+    await newBook.save();
+    res
+      .status(200)
+      .send({ message: 'Livre ajouté avec succès', book: newBook });
+  } catch (error) {
+    console.error("Le livre n'a pas pu être ajouté.", error);
+    res.status(500).send({ message: "Echec lors de l'ajout du livre" });
+  }
+});
 
 module.exports = router;
