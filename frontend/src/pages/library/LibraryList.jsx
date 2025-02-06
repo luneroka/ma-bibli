1ae083ed18b1dc4f4bf4dc0897ea425b5b9272ce
Yoann Robert <185387760+luneroka@users.noreply.github.com>
2025-02-06 14:06:28 +0100

    Style LibraryList

--- frontend/src/pages/library/LibraryList.jsx
+++ frontend/src/pages/library/LibraryList.jsx
@@ -7,7 +7,7 @@ function LibraryList({ libraryBooks = [] }) {
       <div className='items-center gap-8 mt-[64px] mb-[32px]'>
         <h3 className='text-h3 text-black font-merriweather'>Ma Bibli</h3>
       </div>
-      <div className='flex gap-[145px] mt-[32px]'>
+      <div className='flex flex-wrap gap-4 mt-[32px]'>
         {libraryBooks.length > 0 &&
           libraryBooks.map((book) => {
             return <BookCover key={book.id} book={book} />;
