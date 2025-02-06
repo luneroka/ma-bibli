7ee17870625d21c7bb0dc190f63bd713efdd5c70
Yoann Robert <185387760+luneroka@users.noreply.github.com>
2025-02-06 16:11:38 +0100

    Create book schema

---
+++ backend/src/books/book.model.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  publisher: {
+    type: String,
+    required: true,
+  },
+  publishedDate: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  pageCount: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+  },
+  timestamps: true,
+});
