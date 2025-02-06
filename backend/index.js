d1a860751d3dbe78ec923026c1d03f85e2916eba
Yoann Robert <185387760+luneroka@users.noreply.github.com>
2025-02-06 15:40:46 +0100

    Setup mongoGB cluster

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,20 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World! This is my server.');
-});
+const mongoose = require('mongoose');
+
+async function main() {
+  await mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/?retryWrites=true&w=majority&appName=${process.env.MONGO_DB}`
+  );
+  app.use('/', (req, res) => {
+    res.send('Hello World! This is my server.');
+  });
+}
+
+main()
+  .then(() => console.log('MongoDB connected successfully!'))
+  .catch((err) => console.log(err));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
