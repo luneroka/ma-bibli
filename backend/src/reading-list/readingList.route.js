090f6835d144529aac16a3c30a1092210101b53c
Yoann Robert <185387760+luneroka@users.noreply.github.com>
2025-02-08 13:50:15 +0100

    Create get route for ReadingList

--- backend/src/reading-list/readingList.route.js
+++ backend/src/reading-list/readingList.route.js
@@ -3,6 +3,7 @@ const ReadingListBook = require('./readingList.model');
 const { addBook, deleteBook } = require('../book/book.controller');
 const router = express.Router();
 
+router.get('/', (req, res) => getAllBooks(ReadingListBook, req, res));
 router.post('/add-book', (req, res) => addBook(ReadingListBook, req, res));
 router.delete('/delete-book/:bookId', (req, res) =>
   deleteBook(ReadingListBook, req, res)
