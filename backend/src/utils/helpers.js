9ff78fdeeaf245430a2360a943c75595c557219f
Yoann Robert <185387760+luneroka@users.noreply.github.com>
2025-02-10 11:50:34 +0100

    Remove usused helper function

--- backend/src/utils/helpers.js
+++
@@ -1,7 +0,0 @@
-function filterBooksWithImagesAndPages(books) {
-  return books.filter(
-    (book) => book.volumeInfo?.imageLinks && book.volumeInfo?.pageCount > 0
-  );
-}
-
-module.exports = filterBooksWithImagesAndPages;
