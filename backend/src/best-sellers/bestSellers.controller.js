7e71a3f56f141c4d6f24a24714147cb7ca308ead
Yoann Robert <185387760+luneroka@users.noreply.github.com>
2025-02-10 20:05:37 +0100

    Update fetchBestSellers to use fetchIsbnFromGoogle

--- backend/src/best-sellers/bestSellers.controller.js
+++ backend/src/best-sellers/bestSellers.controller.js
@@ -1,9 +1,27 @@
 const { fetchBestSellersFromNYT } = require('../utils/nytBooksApi');
+const { fetchIsbnFromGoogle } = require('../utils/googleBooksApi');
 
 const fetchBestSellers = async (req, res) => {
   try {
-    const bestSellers = await fetchBestSellersFromNYT();
-    res.status(200).json(bestSellers);
+    const nytBestSellers = await fetchBestSellersFromNYT();
+    const bestSellersWithGoogleData = await Promise.all(
+      nytBestSellers.results.books.map(async (book) => {
+        const isbn = book.isbns[0]?.isbn13 || book.isbns[0]?.isbn10;
+        if (isbn) {
+          const googleBookData = await fetchIsbnFromGoogle(isbn);
+          if (googleBookData) {
+            return {
+              ...book,
+              id: googleBookData.id,
+              volumeInfo: googleBookData.volumeInfo,
+            };
+          }
+        }
+        return book;
+      })
+    );
+
+    res.status(200).json(bestSellersWithGoogleData);
   } catch (error) {
     console.error('Failed to fetch best sellers from NYT API', error);
     res
