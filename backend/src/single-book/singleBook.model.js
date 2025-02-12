9b2baf3a01bb9518c4a3492c680be4fb85111a71
Yoann Robert <185387760+luneroka@users.noreply.github.com>
2025-02-12 21:46:22 +0100

    Remove unused singleBook route

--- backend/src/single-book/singleBook.model.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const bookSchema = require('../book/book.model');
-
-const SingleBook = mongoose.model('SingleBook', bookSchema);
-
-module.exports = SingleBook;
--- backend/src/single-book/singleBook.route.js
+++ backend/src/single-book/singleBook.route.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const SingleBook = require('./singleBook.model');
 const { getSingleBook } = require('../book/book.controller');
 const router = express.Router();
 
