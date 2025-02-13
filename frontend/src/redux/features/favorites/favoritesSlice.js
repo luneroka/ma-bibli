6aabc2b5453fef6a1d5a1779628445c965a1695b
Yoann Robert <185387760+luneroka@users.noreply.github.com>
2025-02-13 19:11:15 +0100

    Create extra reducers for getFavoriteBooksAsync

--- frontend/src/redux/features/favorites/favoritesSlice.js
+++ frontend/src/redux/features/favorites/favoritesSlice.js
@@ -1,21 +1,41 @@
-import { toggleFavoriteAsync } from './favoritesAsyncActions';
+import { createSlice } from '@reduxjs/toolkit';
+import {
+  getFavoriteBooksAsync,
+  toggleFavoriteAsync,
+} from './favoritesAsyncActions';
+
+const initialState = {
+  favorites: [],
+  status: 'idle',
+  error: null,
+};
 
 const favoritesSlice = createSlice({
   name: 'favorites',
-  initialState: {
-    favorites: [],
-    loading: false,
-    error: null,
-  },
+  initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder
+      // GET FAVORITE BOOKS
+      .addCase(getFavoriteBooksAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(getFavoriteBooksAsync.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.favorites = action.payload;
+      })
+      .addCase(getFavoriteBooksAsync.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      })
+
+      // TOGGLE FAVORITE
       .addCase(toggleFavoriteAsync.pending, (state) => {
-        state.loading = true;
+        state.status = 'loading';
         state.error = null;
       })
       .addCase(toggleFavoriteAsync.fulfilled, (state, action) => {
-        state.loading = false;
+        state.status = 'succeeded';
         const updatedBook = action.payload;
         state.favorites = state.favorites.filter(
           (book) => book.isbn !== updatedBook.isbn
@@ -25,7 +45,7 @@ const favoritesSlice = createSlice({
         }
       })
       .addCase(toggleFavoriteAsync.rejected, (state, action) => {
-        state.loading = false;
+        state.status = 'failed';
         state.error = action.payload;
       });
   },
