f9c34ac43f0e73b8bffa982901b2f66f8b0dbaae
Yoann Robert <185387760+luneroka@users.noreply.github.com>
2025-02-15 23:04:22 +0100

    Add password visibility toggle to updatePassword input fields

--- frontend/src/pages/user/UserAccount.jsx
+++ frontend/src/pages/user/UserAccount.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useAuth } from '../../context/AuthContext';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 function UserAccount() {
   const {
@@ -12,6 +13,11 @@ function UserAccount() {
     currentUser?.displayName || ''
   );
   const [message, setMessage] = useState('');
+  const [isCurrentPasswordVisible, setIsCurrentPasswordVisible] =
+    useState(false);
+  const [isNewPasswordVisible, setIsNewPasswordVisible] = useState(false);
+  const [isConfirmationPasswordVisible, setisConfirmationPasswordVisible] =
+    useState(false);
 
   const handleUpdateProfile = async (e) => {
     e.preventDefault();
@@ -32,6 +38,26 @@ function UserAccount() {
     }
   };
 
+  const handleUpdatePassword = async (e) => {
+    e.preventDefault();
+  };
+
+  // Toggle current password view
+  const handleToggleCurrentPasswordView = (e) => {
+    e.preventDefault();
+    setIsCurrentPasswordVisible((prev) => !prev);
+  };
+  // Toggle new password view
+  const handleToggleNewCurrentPasswordView = (e) => {
+    e.preventDefault();
+    setIsNewPasswordVisible((prev) => !prev);
+  };
+  // Toggle confirmation password view
+  const handleToggleConfirmationCurrentPasswordView = (e) => {
+    e.preventDefault();
+    setisConfirmationPasswordVisible((prev) => !prev);
+  };
+
   return (
     <div className='flex flex-col flex-1 min-h-0 min-w-[500px] max-w-full mx-auto font-lato'>
       <div className='flex-grow flex items-center justify-center mt-[96px]'>
@@ -40,7 +66,7 @@ function UserAccount() {
             <label className='text-small text-black-75 mb-1'>
               Nom d'utilisateur
             </label>
-            <div className='flex gap-2 mb-2'>
+            <div className='flex gap-2 mb-4'>
               <input
                 type='text'
                 required
@@ -61,7 +87,95 @@ function UserAccount() {
           )}
 
           {/* Update Password */}
+          <form onSubmit={handleUpdatePassword}>
+            <label className='text-small text-black-75 mb-1'>
+              Mot de passe
+            </label>
+            <div className='flex flex-col gap-2 mb-2'>
+              <div className='relative w-[100%] mb-4'>
+                <input
+                  type={isCurrentPasswordVisible ? 'text' : 'password'}
+                  required
+                  placeholder='Mot de passe actuel'
+                  className='font-merriweather text-small text-black-75 shadow border border-black-25 focus:outline-secondary-btn w-full py-2 px-3'
+                />
 
+                {isCurrentPasswordVisible ? (
+                  <button
+                    type='button'
+                    onClick={handleToggleCurrentPasswordView}
+                    className='cursor-pointer absolute right-3 inset-y-0 my-auto text-black-50'
+                  >
+                    <FaEyeSlash />
+                  </button>
+                ) : (
+                  <button
+                    type='button'
+                    onClick={handleToggleCurrentPasswordView}
+                    className='cursor-pointer absolute right-3 inset-y-0 my-auto text-black-50'
+                  >
+                    <FaEye />
+                  </button>
+                )}
+              </div>
+              <div className='relative w-[100%] mb-4'>
+                <input
+                  type={isNewPasswordVisible ? 'text' : 'password'}
+                  required
+                  placeholder='Nouveau mot de passe'
+                  className='font-merriweather text-small text-black-75 shadow border border-black-25 focus:outline-secondary-btn w-full py-2 px-3'
+                />
+                {isNewPasswordVisible ? (
+                  <button
+                    type='button'
+                    onClick={handleToggleNewCurrentPasswordView}
+                    className='cursor-pointer absolute right-3 inset-y-0 my-auto text-black-50'
+                  >
+                    <FaEyeSlash />
+                  </button>
+                ) : (
+                  <button
+                    type='button'
+                    onClick={handleToggleNewCurrentPasswordView}
+                    className='cursor-pointer absolute right-3 inset-y-0 my-auto text-black-50'
+                  >
+                    <FaEye />
+                  </button>
+                )}
+              </div>
+              <div className='relative w-[100%] mb-4'>
+                <input
+                  type={isConfirmationPasswordVisible ? 'text' : 'password'}
+                  required
+                  placeholder='Confirmer nouveau mot de passe'
+                  className='font-merriweather text-small text-black-75 shadow border border-black-25 focus:outline-secondary-btn w-full py-2 px-3'
+                />
+                {isConfirmationPasswordVisible ? (
+                  <button
+                    type='button'
+                    onClick={handleToggleConfirmationCurrentPasswordView}
+                    className='cursor-pointer absolute right-3 inset-y-0 my-auto text-black-50'
+                  >
+                    <FaEyeSlash />
+                  </button>
+                ) : (
+                  <button
+                    type='button'
+                    onClick={handleToggleConfirmationCurrentPasswordView}
+                    className='cursor-pointer absolute right-3 inset-y-0 my-auto text-black-50'
+                  >
+                    <FaEye />
+                  </button>
+                )}
+              </div>
+              <button
+                type='submit'
+                className='cursor-pointer bg-primary-btn hover:bg-secondary-btn active:bg-black-75 text-white p-2 w-33'
+              >
+                Modifier
+              </button>
+            </div>
+          </form>
           {/* Delete Account */}
         </div>
       </div>
