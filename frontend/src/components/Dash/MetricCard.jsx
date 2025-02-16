1b221a619f5d834d43b452954d4b6b0505f7f3cb
Yoann Robert <185387760+luneroka@users.noreply.github.com>
2025-02-16 17:03:48 +0100

    Create MetricCard

--- frontend/src/components/Dash/MetricCard.jsx
+++ frontend/src/components/Dash/MetricCard.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 function MetricCard() {
   return (
-    <div>MetricCard</div>
-  )
+    <div className='h-[140px] pl-[64px] bg-white-bg flex flex-col justify-center'>
+      <p className='text-metrics-number text-secondary-btn'>223</p>
+      <p className='text-metrics-type'>Livres</p>
+    </div>
+  );
 }
 
-export default MetricCard
+export default MetricCard;
