c33f0465ff2374b9c5d31fd6282d9039a1bf3f53
Yoann Robert <185387760+luneroka@users.noreply.github.com>
2025-02-17 12:03:35 +0100

    Create genre filter button in Library

--- frontend/src/pages/library/LibraryList.jsx
+++ frontend/src/pages/library/LibraryList.jsx
@@ -4,11 +4,14 @@ import BookInLibrary from '../../components/Book/BookInLibrary';
 function LibraryList({ libraryBooks = [] }) {
   return (
     <>
-      <div className='items-center gap-8 mt-[64px] mb-[32px]'>
+      <div className='flex justify-between items-center gap-8 mt-[64px] mb-[32px]'>
         <h3 className='text-h3 text-black font-merriweather'>Ma Bibli</h3>
+        <select className='py-1 px-4 bg-secondary-btn text-white-bg w-[200px]'>
+          {/* Map options */}
+        </select>
       </div>
 
-      <div className='flex flex-wrap gap-4 mt-[32px]'>
+      <div className='flex flex-wrap gap-4 justify-between mt-[32px]'>
         {libraryBooks.length > 0 &&
           libraryBooks.map((book) => {
             return <BookInLibrary key={book.isbn} book={book} />;
