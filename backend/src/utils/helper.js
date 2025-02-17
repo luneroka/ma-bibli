111cc03fc5e54932a352f1865fadd2df6793adf2
Yoann Robert <185387760+luneroka@users.noreply.github.com>
2025-02-17 11:39:17 +0100

    Create mapBookCategory helper

--- backend/src/utils/helper.js
+++ backend/src/utils/helper.js
@@ -8,4 +8,75 @@ const fetchJson = async (url) => {
   return response.json();
 };
 
-module.exports = { fetchJson };
+function mapBookCategory(category) {
+  const categoryMapping = {
+    'ANTIQUES & COLLECTIBLES': 'Art & Design',
+    ARCHITECTURE: 'Art & Design',
+    ART: 'Art & Design',
+    PHOTOGRAPHY: 'Art & Design',
+    DESIGN: 'Art & Design',
+
+    'LITERARY COLLECTIONS': 'Littérature',
+    'LITERARY CRITICISM': 'Littérature',
+    POETRY: 'Littérature',
+    FICTION: 'Littérature',
+    DRAMA: 'Littérature',
+
+    'BIOGRAPHY & AUTOBIOGRAPHY': 'Biographie & Mémoires',
+    'TRUE CRIME': 'Biographie & Mémoires',
+
+    'BUSINESS & ECONOMICS': 'Affaires & Finance',
+    'POLITICAL SCIENCE': 'Affaires & Finance',
+    LAW: 'Affaires & Finance',
+
+    COMPUTERS: 'Science & Technologie',
+    'TECHNOLOGY & ENGINEERING': 'Science & Technologie',
+    SCIENCE: 'Science & Technologie',
+    MATHEMATICS: 'Science & Technologie',
+
+    'BODY, MIND & SPIRIT': 'Développement Personnel & Bien-être',
+    'SELF-HELP': 'Développement Personnel & Bien-être',
+    'HEALTH & FITNESS': 'Développement Personnel & Bien-être',
+    PSYCHOLOGY: 'Développement Personnel & Bien-être',
+
+    'FAMILY & RELATIONSHIPS': 'Mode de Vie',
+    'HOUSE & HOME': 'Mode de Vie',
+    GARDENING: 'Mode de Vie',
+    COOKING: 'Mode de Vie',
+    PETS: 'Mode de Vie',
+
+    'GAMES & ACTIVITIES': 'Divertissement',
+    HUMOR: 'Divertissement',
+    'SPORTS & RECREATION': 'Divertissement',
+    'PERFORMING ARTS': 'Divertissement',
+    MUSIC: 'Divertissement',
+    'COMICS & GRAPHIC NOVELS': 'Divertissement',
+
+    EDUCATION: 'Éducation & Référence',
+    'STUDY AIDS': 'Éducation & Référence',
+    REFERENCE: 'Éducation & Référence',
+    'LANGUAGE ARTS & DISCIPLINES': 'Éducation & Référence',
+    'LANGUAGE STUDY': 'Éducation & Référence',
+
+    PHILOSOPHY: 'Sciences Sociales & Culturelles',
+    RELIGION: 'Sciences Sociales & Culturelles',
+    'SOCIAL SCIENCE': 'Sciences Sociales & Culturelles',
+    HISTORY: 'Sciences Sociales & Culturelles',
+    TRAVEL: 'Sciences Sociales & Culturelles',
+
+    'JUVENILE FICTION': 'Jeunesse & Young Adult',
+    'JUVENILE NONFICTION': 'Jeunesse & Young Adult',
+    'YOUNG ADULT FICTION': 'Jeunesse & Young Adult',
+    'YOUNG ADULT NONFICTION': 'Jeunesse & Young Adult',
+
+    MEDICAL: 'Sujets Spécialisés',
+    NATURE: 'Sujets Spécialisés',
+    TRANSPORTATION: 'Sujets Spécialisés',
+    'CRAFTS & HOBBIES': 'Sujets Spécialisés',
+    BIBLES: 'Sujets Spécialisés',
+  };
+
+  return categoryMapping[category.toUpperCase()] || 'Non catégorisé';
+}
+
+module.exports = { fetchJson, mapBookCategory };
