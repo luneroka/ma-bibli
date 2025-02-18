c4afc73876394be5adc7872b755d4eca79bace1d
Yoann Robert <185387760+luneroka@users.noreply.github.com>
2025-02-18 14:03:42 +0100

    Make ListItem title a clickable link to SingleBookPage

--- frontend/src/components/Dash/ListItem.jsx
+++ frontend/src/components/Dash/ListItem.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function ListItem({ book }) {
   return (
     <div className='text-start flex flex-col m-0'>
       <div className='pl-2 text-black text-small-body font-light'>
-        {book.title}
+        <Link to={`/livres/${book.isbn}`}>{book.title}</Link>
       </div>
       <div className='pl-2 text-black-50 text-small font-light'>
         {book.authors}
