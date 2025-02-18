25ed950fa865ffc7ec290f38162e17cd800246fc
Yoann Robert <185387760+luneroka@users.noreply.github.com>
2025-02-18 13:53:59 +0100

    Make dashboard responsive

--- frontend/src/pages/dashboard/DashBody.jsx
+++ frontend/src/pages/dashboard/DashBody.jsx
@@ -8,7 +8,7 @@ function DashBody() {
   const libraryBooks = useSelector((state) => state.library.libraryBooks) || [];
 
   return (
-    <div className='mx-[128px] grid grid-cols-4 gap-[32px]'>
+    <div className='mx-[64px] md:mx-[128px] grid grid-cols-1 min-[700px]:grid-cols-2 lg:grid-cols-4 gap-[32px]'>
       {/* Metrics Cards */}
       <MetricCard variant='books' libraryBooks={libraryBooks} />
       <MetricCard variant='pageCount' libraryBooks={libraryBooks} />
@@ -16,19 +16,13 @@ function DashBody() {
       <MetricCard variant='topAuthor' libraryBooks={libraryBooks} />
 
       {/* Graph Card */}
-      <div className='col-span-2'>
+      <div className='col-span-1 min-[700px]:col-span-2 lg:col-span-2'>
         <GraphCard libraryBooks={libraryBooks} />
       </div>
 
       {/* List Card */}
-      <ListCard
-        variant='recent'
-        libraryBooks={libraryBooks}
-      />
-      <ListCard
-        variant='favorites'
-        libraryBooks={libraryBooks}
-      />
+      <ListCard variant='recent' libraryBooks={libraryBooks} />
+      <ListCard variant='favorites' libraryBooks={libraryBooks} />
     </div>
   );
 }
--- frontend/src/pages/dashboard/DashHeader.jsx
+++ frontend/src/pages/dashboard/DashHeader.jsx
@@ -3,12 +3,14 @@ import { FaPen } from 'react-icons/fa';
 
 function DashHeader() {
   return (
-    <div className='flex justify-between items-center mx-[128px] mt-[64px] mb-[32px]'>
+    <div className='flex flex-col md:flex-row justify-between items-center mx-4 md:mx-[128px] mt-4 md:mt-[64px] mb-4 md:mb-[32px]'>
       {/* Title */}
-      <h3 className='text-h3 text-black font-merriweather'>Dashboard</h3>
+      <h3 className='text-h4 md:text-h3 text-black font-merriweather'>
+        Dashboard
+      </h3>
 
       {/* Filters */}
-      <div className='flex gap-4 text-black-50'>
+      <div className='flex gap-4 text-sm md:text-base text-black-50 my-2 md:my-0'>
         <p>Total</p>
         <p>|</p>
         <p>6 derniers mois</p>
@@ -19,9 +21,9 @@ function DashHeader() {
       {/* Library Button */}
       <a
         href='/bibli'
-        className='font-merriweather text-h6 font-light flex gap-2 py-2 px-4 bg-primary-btn items-center text-black-75 hover:bg-secondary-btn active:bg-black-75 active:text-white-bg'
+        className='font-merriweather text-body md:text-body font-light flex gap-1 md:gap-2 py-1 md:py-2 px-2 md:px-4 bg-primary-btn items-center text-black-75 hover:bg-secondary-btn active:bg-black-75 active:text-white-bg'
       >
-        <FaPen />
+        <FaPen className='text-xs md:text-base' />
         Gérer mes livres
       </a>
     </div>
