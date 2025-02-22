aae2ce6a61f1796333a258c9db1cc149a15f2571
Yoann Robert <185387760+luneroka@users.noreply.github.com>
2025-02-22 14:12:41 +0100

    Display read books data instead of all books in metrics cards

--- frontend/src/components/Dash/MetricCard.jsx
+++ frontend/src/components/Dash/MetricCard.jsx
@@ -5,13 +5,26 @@ import {
 } from '../../utils/helper';
 
 function MetricCard({ variant, libraryBooks = [] }) {
+  const haveReadBooks = [...libraryBooks].filter(
+    (book) => book.haveRead === true
+  );
+
   if (variant === 'books') {
     return (
-      <div className='h-[120px] pl-[64px] bg-white-bg flex flex-col justify-center shadow-lg'>
-        <p className='text-metrics-number text-secondary-btn'>
-          {libraryBooks.length}
-        </p>
-        <p className='text-metrics-type font-light'>Livres</p>
+      <div className='h-[120px] pl-[64px] pr-[96px] bg-white-bg flex justify-between items-center shadow-lg'>
+        <div className=''>
+          <p className='text-metrics-number text-secondary-btn'>
+            {libraryBooks.length}
+          </p>
+          <p className='text-metrics-type font-light'>Livres</p>
+        </div>
+        <div className='text-h1 font-thin text-black-50'>|</div>
+        <div>
+          <p className='text-metrics-number text-secondary-btn'>
+            {haveReadBooks.length}
+          </p>
+          <p className='text-metrics-type font-light'>Lus</p>
+        </div>
       </div>
     );
   } else if (variant === 'pageCount') {
@@ -20,10 +33,18 @@ function MetricCard({ variant, libraryBooks = [] }) {
         ? 0
         : libraryBooks.reduce((total, book) => total + book.pageCount, 0);
 
+    const totalReadPages =
+      haveReadBooks.length === 0
+        ? 0
+        : haveReadBooks.reduce((total, book) => total + book.pageCount, 0);
+
     return (
       <div className='h-[120px] pl-[64px] bg-white-bg flex flex-col justify-center shadow-lg'>
-        <p className='text-metrics-number text-secondary-btn'>
+        {/* <p className='text-metrics-number text-secondary-btn'>
           {totalPages.toLocaleString('fr-FR')}
+        </p> */}
+        <p className='text-metrics-number text-secondary-btn'>
+          {totalReadPages.toLocaleString('fr-FR')}
         </p>
         <p className='text-metrics-type font-light'>Pages lues</p>
       </div>
